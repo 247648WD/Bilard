49bd3bb9f9f5d84a528acf3cc264c9e4712f5258
247648WD <247648@edu.p.lodz.pl>
2024-03-18 15:17:52 +0100

    Bila

---
+++ Bila.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Bila
+{
+    private double posX;
+    private double posY;
+    private int vel;
+    private int dir;
+
+    public Bila(double posX, double posY, int vel, int dir)
+    {
+        this.posX = posX;
+        this.posY = posY;
+        this.vel = vel;
+        this.dir = dir;
+    }
+
+    public double getX() { return posX; }
+    public double getY() { return posY; }
+    public int getVel() { return vel; }
+    public int getDir() { return dir; }
+
+    private void setX(double x) { this.posX = x; }
+    private void setY(double y) { this.posY = y; }
+    private void setVel(int vel) { this.vel = vel; }
+    private void setDir(int dir) { this.dir = dir; }
+
+    public void updatePos()
+    {
+        switch (dir)
+        {
+            case 0:
+                setY(getY() + vel); break;
+            case 90:
+                setX(getX() + vel); break;
+            case 180:
+                setY(getY() - vel); break;
+            case 270:
+                setX(getX() - vel); break;
+            default:
+                break;
+        }
+
+
+    }
+
+}
