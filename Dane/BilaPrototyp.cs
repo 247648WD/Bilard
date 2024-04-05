9c26318640a43d5c3f6129ba44fc59fbdf936ef4
247648WD <247648@edu.p.lodz.pl>
2024-04-05 13:02:07 +0200

    Dodane dane, logika, widok Prototyp Bili

---
+++ Dane/BilaPrototyp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    internal class BilaPrototyp
+    {
+        private double posX;
+        private double posY;
+        private double mass;
+        private int size;
+        private int vel;
+        private int dir;
+
+        public BilaPrototyp Copy(double posX, double posY, double mass, int size, int vel, int dir)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.vel = vel;
+            this.dir = dir;
+            this.mass = mass;
+            this.size = size;
+            return this;
+        }
+    }
+}
