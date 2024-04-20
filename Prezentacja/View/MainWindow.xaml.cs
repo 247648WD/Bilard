774a0523162840e248b65d2d70f884b3d0de70a1
247630AB <247630@edu.p.lodz.pl>
2024-04-20 20:34:47 +0200

    dodawanie bili, należy poprawić współrzędne po których może się poruszać

--- Prezentacja/View/MainWindow.xaml.cs
+++ Prezentacja/View/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Prezentacja
         private int boundariesRight = 420;
         private int boundariesLeft = -215;
         private int vx = 5;
-
+        private List<Ellipse> ellipseList = new List<Ellipse>();
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             DispatcherTimer timer = new DispatcherTimer();
@@ -36,11 +36,46 @@ namespace Prezentacja
 
         private void timerTicker(object sender, EventArgs e)
         {
-            Canvas.SetLeft(bila, Canvas.GetLeft(bila) + vx);
-            if (Canvas.GetLeft(bila) == boundariesRight || Canvas.GetLeft(bila) == boundariesLeft)
+            for (int i = 0; i < ellipseList.Count; i++)
             {
-                vx *= -1;
+                double left = Canvas.GetLeft(ellipseList[i]);
+                Canvas.SetLeft(ellipseList[i], left + vx);
+
+                // Sprawdź, czy bila dotarła do granic obszaru zielonego
+                if (left <= boundariesLeft || left + ellipseList[i].Width >= boundariesRight)
+                {
+                    vx *= -1;
+                }
             }
+        }
+        private void add_bila(object sender, RoutedEventArgs e)
+        {
+            // Pobierz wymiary obszaru zielonego
+            double greenAreaWidth = 700;
+            double greenAreaHeight = 350;
+
+            // Oblicz granice obszaru zielonego
+            double leftBoundary = 30;
+            double topBoundary = 10;
+            double rightBoundary = leftBoundary + greenAreaWidth;
+            double bottomBoundary = topBoundary + greenAreaHeight;
+
+            // Tworzenie nowej elipsy
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 63;
+            ellipse.Height = 59;
+            ellipse.Fill = Brushes.Beige;
+            ellipse.Stroke = Brushes.Black;
+
+            // Ustawienie losowej pozycji dla elipsy w obrębie obszaru zielonego
+            Random random = new Random();
+            double randomLeft = random.NextDouble() * (rightBoundary - leftBoundary) + leftBoundary;
+            double randomTop = random.NextDouble() * (bottomBoundary - topBoundary) + topBoundary;
+            Canvas.SetLeft(ellipse, randomLeft);
+            Canvas.SetTop(ellipse, randomTop);
+            ellipseList.Add(ellipse);
+            // Dodanie elipsy do Canvas
+            canvas.Children.Add(ellipse);
         }
 
     }
